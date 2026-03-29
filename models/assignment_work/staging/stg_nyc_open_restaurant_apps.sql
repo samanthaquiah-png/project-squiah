0a4ed92aa46991e3ba7ddbd8ab9273fa99384c7b
Samantha Quiah <samantha.quiah@gmail.com>
2026-03-29 18:24:16 +0000

    Updated the files to fix errors

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -2,7 +2,7 @@
 -- One row per restaurant application
 
 WITH source AS (
-    SELECT * FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+    SELECT * FROM {{ source('raw_restaurants', 'source_nyc_open_restaurant_apps') }}
 ), -- Easier to refer to the dbt reference to a long name table this way
 
 cleaned AS (
